0462a31f3db47b782e332a991ab142ea90c17aae
Robert Cowher <bobcowher@gmail.com>
2024-09-29 20:20:43 -0500

    starter code for the password cracker.

---
+++ README.md
@@ -0,0 +1,6 @@
+victim2:aaa
+victim1:password123
+victim:password123
+
+Password list: https://raw.githubusercontent.com/danielmiessler/SecLists/refs/heads/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <crypt.h>
+#include <cstring>
+#include <fstream>
+#include <vector>
+#include <string>
+
+const std::string CHARSET = "abcdefghijklmnopqrstuvwxyz"
+                            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                            "0123456789";
